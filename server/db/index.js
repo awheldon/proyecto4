654af64fc57d1993b04f138c92b271d4dcbbec8b
Alex Wheldon <a.wheldon@gmail.com>
2020-11-17 14:52:10 +0000

    heroku deploy

--- server/db/index.js
+++ server/db/index.js
@@ -5,8 +5,8 @@ const { Pool } = pg;
 // ! Cambiar los datos de la linea 8 para que este correcto en tu local
 
 const pool = new Pool({
-  connectionString:
-    'postgresql://oscar_german:valerialigiasami@localhost:5432/recetario',
+  connectionString: process.env.DATABASE_URL ||
+    'postgresql://alexwheldon:1234@localhost:5432/recetario',
 });
 export const query = async ({ tag = '', queryString, params }) => {
   try {
